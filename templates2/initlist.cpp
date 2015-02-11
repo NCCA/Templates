444e2b3bad54d85164f89b1088f8357cc9043da6
jmacey <jmacey@bournemouth.ac.uk>
2015-02-11 16:15:12 +0000

    added another simple init list demo

---
+++ templates2/initlist.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <initializer_list>
+
+template <typename T>
+void print(std::initializer_list<T> data)
+{
+	for(auto s : data)
+		std::cout<<s<<" ";
+	std::cout<<std::endl;
+}
+
+int main()
+{
+	print({"this","and","that"});
+	print({1,2,3,4,5});
+	print({1.0f,2.0f,0.2f,9.1f});
+	print({'a','b','c'});
+	return EXIT_SUCCESS;
+}
